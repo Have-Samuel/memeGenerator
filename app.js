3fab4d5b922ecc62509e01abef60c8a86bb29267
Have Samuel <samiehave9@gmail.com>
2023-02-18 03:51:05 +0300

    Create the JS file and a Little styling

---
+++ app.js
@@ -0,0 +1,7 @@
+const memeForm = document.querySelector('#memeForm');
+const addMemeBtn = document.querySelector('#memeOutput');
+
+memeForm.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('HEY!!');
+});
