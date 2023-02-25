07815ea7ff3984701abe754c4bdf2c2c6777ce63
Have Samuel <samiehave9@gmail.com>
2023-02-26 01:19:37 +0300

    Changing the HTML and writing the javascript

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const memeForm = document.querySelector('#memeForm');
 const urlInput = document.querySelector('input[name="memeURL"]');
 const topTextInput = document.querySelector('input[name="memeTopText"]');
-const downTextInput = document.querySelector('input[name="memeDownText"]');
+const bottomTextInput = document.querySelector('input[name="memeBottomText"]');
 const outPut = document.querySelector('#memeOutput');
 // const addMemeBtn = document.querySelector('#memeOutput');
 
-function imageCreator(imgURL, text1, text2) {
+function imageCreator(imgURL, topText, bottomText) {
   const memeDiv = document.createElement('div');
   const memePic = document.createElement('image');
   // const memeText = document.createElement('text');
   memePic.innerText = imgURL;
   memeDiv.style.position = 'relative';
   memePic.innerHTML = imgURL.value;
-  memePic.innerText = text1;
-  memePic.innerText = text2;
+  memePic.innerText = topText;
+  memePic.innerText = bottomText;
   memePic.style.fontSize = '80px';
   memePic.style.borderRadius = '8px';
   memePic.style.color = 'magenta';
@@ -26,7 +26,7 @@ memeForm.addEventListener('submit', (e) => {
   const newElemet = imageCreator(
     urlInput.value,
     topTextInput.value,
-    downTextInput.value,
+    bottomTextInput.value,
   );
   outPut.appendChild(newElemet);
 });
