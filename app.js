fbd66c1195d176e94a7fd0ef8f0bdee0ede224d7
Have Samuel <samiehave9@gmail.com>
2023-02-26 01:00:50 +0300

    Making the Generator interactive

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ function imageCreator(imgURL, text1, text2) {
   const memePic = document.createElement('image');
   // const memeText = document.createElement('text');
   memePic.innerText = imgURL;
-  // memeDiv.style.position = 'relative';
-  // memePic.innerHTML = imgURL.value;
+  memeDiv.style.position = 'relative';
+  memePic.innerHTML = imgURL.value;
   memePic.innerText = text1;
   memePic.innerText = text2;
   memePic.style.fontSize = '80px';
@@ -30,3 +30,24 @@ memeForm.addEventListener('submit', (e) => {
   );
   outPut.appendChild(newElemet);
 });
+
+// const imageUrlInput = document.querySelector('#imgUrlInput');
+// const topTextInput = document.querySelector('#topTextInput');
+// const bottomTextInput = document.querySelector('#bottomTextInput');
+// const canvas = document.querySelector('#memeOutput');
+// const addMemes = document.querySelector('type=["submit"]');
+
+// let image;
+
+// imageUrlInput.addEventListener('change', (e) => {
+//   const imageDataUrl = URL.createObjectURL(e.target.URL);
+
+//   console.log(imageDataUrl);
+// });
+
+// function updateMemeCavas(canvas, imgURL, topText, bottomText) {
+//   console.log(canvas);
+//   console.log(canvas);
+//   console.log(top);
+//   console.log(bottomText);
+// }
