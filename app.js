b27df1cbb22953c27b51f6ba03252cb4b2f3dc3b
Have Samuel <samiehave9@gmail.com>
2023-03-09 19:33:30 +0300

    intersctive

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const memeForm = document.querySelector('#memeForm');
-// const urlInput = document.querySelector('input[name="memeURL"]');
-// const topTextInput = document.querySelector('input[name="memeTopText"]');
-// const bottomTextInput = document.querySelector('input[name="memeBottomText"]');
-// const memeDisplay = document.querySelector('#memeOutput');
-// const addMemeBtn = document.querySelector('#memeOutput');
+const urlInput = document.querySelector('input[name="memeURL"]');
+const topTextInput = document.querySelector('input[name="memeTopText"]');
+const bottomTextInput = document.querySelector('input[name="memeBottomText"]');
+const memeDisplay = document.querySelector('#memeOutput');
+const addMemeBtn = document.querySelector('#memeOutput');
+
+let src = memeDisplay.getAttribute('src');
+src = 'https://i.imgflip.com/1bij.jpg';
+urlInput.setAttribute('src', src);
+
+memeDisplay.setAttribute('src', src);
+memeDisplay.classList = 'memeImg';
 
 memeForm.addEventListener('submit', (e) => {
   e.preventDefault();
