a98aa3dae14e1aa19093fa6e74837d5cc9c0dd2b
Have Samuel <samiehave9@gmail.com>
2023-03-09 18:04:12 +0300

    working on the form

--- app.js
+++ app.js
@@ -5,49 +5,7 @@ const bottomTextInput = document.querySelector('input[name="memeBottomText"]');
 const memeDisplay = document.querySelector('#memeOutput');
 // const addMemeBtn = document.querySelector('#memeOutput');
 
-function imageCreator(imgURL, topText, bottomText) {
-  const memeDiv = document.createElement('div');
-  const memePic = document.createElement('img');
-  // const memeText = document.createElement('text');
-  // memePic.innerText = imgURL;
-  // memeDiv.style.position = 'relative';
-  // memePic.innerHTML = imgURL.value;
-  memePic.innerText = topText;
-  memePic.innerText = bottomText;
-  memePic.style.fontSize = '80px';
-  memePic.style.borderRadius = '8px';
-  memePic.style.color = 'magenta';
-  memeDiv.appendChild(memePic);
-  return memeDiv;
-}
-
 memeForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const newElemet = imageCreator(
-    urlInput.value,
-    topTextInput.value,
-    bottomTextInput.value,
-  );
-  memeDisplay.appendChild(newElemet);
+
 });
-
-// const imageUrlInput = document.querySelector('#imgUrlInput');
-// const topTextInput = document.querySelector('#topTextInput');
-// const bottomTextInput = document.querySelector('#bottomTextInput');
-// const canvas = document.querySelector('#memeOutput');
-// const addMemes = document.querySelector('type=["submit"]');
-
-// let image;
-
-// imageUrlInput.addEventListener('change', (e) => {
-//   const imageDataUrl = URL.createObjectURL(e.target.URL);
-
-//   console.log(imageDataUrl);
-// });
-
-// function updateMemeCavas(canvas, imgURL, topText, bottomText) {
-//   console.log(canvas);
-//   console.log(canvas);
-//   console.log(top);
-//   console.log(bottomText);
-// }
