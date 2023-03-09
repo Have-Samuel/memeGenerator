d93c249a2969fac4523e5ce1b5403f6a4ba22da7
Have Samuel <samiehave9@gmail.com>
2023-03-10 02:28:17 +0300

    Styling the Meme

--- app.js
+++ app.js
@@ -3,16 +3,38 @@ const urlInput = document.querySelector('input[name="memeURL"]');
 const topTextInput = document.querySelector('input[name="memeTopText"]');
 const bottomTextInput = document.querySelector('input[name="memeBottomText"]');
 const memeDisplay = document.querySelector('#memeOutput');
-const addMemeBtn = document.querySelector('#memeOutput');
 
-let src = urlInput.getAttribute('src');
-src = 'https://i.imgflip.com/1bij.jpg';
-urlInput.setAttribute('src', src);
+const memeGenetator = () => {
+  const memeURL = urlInput.value;
+  const topText = topTextInput.value;
+  const bottomText = bottomTextInput.value;
 
+  // memeDisplay.innerHTML = '';
 
-memeDisplay.setAttribute('src', src);
-memeDisplay.classList = 'memeImg';
+  const memeDiv = document.createElement('div');
+  // memeDiv.classList.add('');
+
+  const memeImage = document.createElement('img');
+  memeImage.src = memeURL;
+  // memeImage.classList.add('memeImg');
+  memeImage.alt = 'Meme Image';
+
+  const memeTopText = document.createElement('h2');
+  memeTopText.classList.add('top');
+  memeTopText.innerText = topText;
+
+  const memeBottomText = document.createElement('h2');
+  memeBottomText.classList.add('bottom');
+  memeBottomText.innerText = bottomText;
+
+  memeDiv.append(memeImage, memeTopText, memeBottomText);
+  memeDisplay.appendChild(memeDiv);
+
+  // memeForm.reset();
+};
 
 memeForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  memeGenetator();
 });
