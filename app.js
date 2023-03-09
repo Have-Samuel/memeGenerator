c44e7b60fcce0d3b413137710410cff67b19bc33
Have Samuel <samiehave9@gmail.com>
2023-03-09 18:15:10 +0300

    Fix Linter

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const memeForm = document.querySelector('#memeForm');
-const urlInput = document.querySelector('input[name="memeURL"]');
-const topTextInput = document.querySelector('input[name="memeTopText"]');
-const bottomTextInput = document.querySelector('input[name="memeBottomText"]');
-const memeDisplay = document.querySelector('#memeOutput');
+// const urlInput = document.querySelector('input[name="memeURL"]');
+// const topTextInput = document.querySelector('input[name="memeTopText"]');
+// const bottomTextInput = document.querySelector('input[name="memeBottomText"]');
+// const memeDisplay = document.querySelector('#memeOutput');
 // const addMemeBtn = document.querySelector('#memeOutput');
 
 memeForm.addEventListener('submit', (e) => {
   e.preventDefault();
-
 });
